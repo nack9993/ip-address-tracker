edaf1f24825ff4470478d460803669e45aa98384
Nack <nack@buzzwoo.de>
2023-09-11 14:19:07 +0700

    Fix the deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { cjsInterop } from "vite-plugin-cjs-interop";
 
 export default {
+  ssr: {
+    noExternal: ["@googlemaps/js-api-loader"],
+  },
   plugins: [
     cjsInterop({
       dependencies: ["@googlemaps/js-api-loader"],
